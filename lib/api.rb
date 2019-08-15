ffbed42f968836218b0d1f5dcf66b78e47cb59db
lowlandjuju <mp@fixedabode.cc>
2019-08-15 18:47:42 +0200

    Incorrect return object for crud methods

--- lib/api.rb
+++ lib/api.rb
@@ -29,17 +29,17 @@ module Speakout
 
     def get(path)
       response = @client.get("#{base_url}/#{path}", nil, headers)
-      [404, 500].include?(response.status) ? nil : [JSON.parse(response.body), response.status]
+      [[404, 500].include?(response.status) ? nil : JSON.parse(response.body), response.status]
     end
 
     def post(path, data = {})
       response = @client.post("#{base_url}/#{path}", data, headers)
-      [404, 500].include?(response.status) ? nil : [JSON.parse(response.body), response.status]
+      [[404, 500].include?(response.status) ? nil : JSON.parse(response.body), response.status]
     end
 
     def put(path, data = {})
       response = @client.put("#{base_url}/#{path}", data.to_json, headers)
-      [404, 500].include?(response.status) ? nil : [JSON.parse(response.body), response.status]
+      [[404, 500].include?(response.status) ? nil : JSON.parse(response.body), response.status]
     end
 
     private
